367969e0e5dc09f5d1cf2dcf36bd0ed22d7c1c1f
taesoku <benjamin.han@bidenergy.com>
2017-04-24 15:05:14 +1000

    01.Amazon-01.Get.Binary.Tree.Right.Side.View

---
+++ 01.Amazon/01.Get.Binary.Tree.Right.Side.View.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace _01.Amazon
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class _01GetBinaryTreeRightSideView
+    {
+
+        public static void Answer()
+        {
+            var root = new TreeNode(1);
+            root.right = new TreeNode(2);
+            var output = RightSideView(root);
+        }
+
+        private static List<int> RightSideView(TreeNode root, List<int> outputs = null, int depth = 0)
+        {
+            if (outputs == null) outputs = new List<int>();
+            if (root == null) return outputs;
+            if (depth == outputs.Count)
+                outputs.Add(root.val);
+            RightSideView(root.right, outputs, depth + 1);
+            RightSideView(root.left, outputs, depth + 1);
+            return outputs;
+        }
+    }
+
+}
---
+++ 01.Amazon/Amazon.cs
@@ -0,0 +1,22 @@
+﻿using _01.Amazon;
+
+namespace _04.Test
+{
+    class Amazon
+    {
+
+        // count: return int - counting
+        // find: return object
+        // get: return array || list
+        // is: return bool
+        // max: return int - finding maximum
+        // min: return int - finding minimum
+        // print: return string
+        // run: return void
+
+        static void Main(string[] args)
+        {
+            _01GetBinaryTreeRightSideView.Answer();
+        }
+    }
+}
