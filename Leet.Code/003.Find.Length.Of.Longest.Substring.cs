036a2ea481be1c56e5a33af5069c0b07aeb3201a
taesoku <benjamin.han@bidenergy.com>
2017-12-04 09:19:48 +1100

    - Find Length of Longest Substring : added

---
+++ Leet.Code/003.Find.Length.Of.Longest.Substring.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leet.Code
+{
+
+    public class _003FindLengthOfLongestSubstring
+    {
+
+        public static void Answer()
+        {
+            var output = FindLengthOfLongestSubstring("abcabcbb");
+        }
+
+        public static int FindLengthOfLongestSubstring(string input)
+        {
+            var hashtable = new Hashtable();
+            var max = 0;
+            var n = input.Length;
+            for (int i = 0, j = 0; j < n; j++)
+            {
+                if (hashtable.ContainsKey(input[j]))
+                    i = Math.Max((int)hashtable[input[j]], i);
+                max = Math.Max(max, j - i + 1);
+                hashtable[input[j]] = j + 1;
+            }
+            return max;
+        }
+
+    }
+
+}
--- Leet.Code/Leet.Code.cs
+++ Leet.Code/Leet.Code.cs
@@ -16,7 +16,8 @@
         static void Main()
         {
             //_001GetTwoSum.Answer();
-            _002GetTwoNumbersAddition.Answer();
+            //_002GetTwoNumbersAddition.Answer();
+            _003FindLengthOfLongestSubstring.Answer();
             //_01GetBinaryTreeRightSideView.Answer();
             //_02DesignTwitter.Answer();
             //_03GetReverseWords.Answer();
