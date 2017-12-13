517de425f5bd4c2a1f5c7c67d04d69f679099728
taesoku <benjamin.han@bidenergy.com>
2017-12-13 17:04:33 +1100

    - Find Roman To Int : updated

---
+++ Leet.Code/013.Find.RomanTo.Int.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Leet.Code
+{
+
+    public class _0013FindRomanToInt
+    {
+
+        public static void Answer()
+        {
+            var output = FindRomanToInt("MM");
+        }
+
+        public static Dictionary<char, int> dictionary = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        };
+
+        public static int FindRomanToInt(string input)
+        {
+            var output = 0;
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (i + 1 < input.Length && dictionary[input[i]] < dictionary[input[i + 1]])
+                    output -= dictionary[input[i]];
+                else output += dictionary[input[i]];
+            }
+            return output;
+        }
+
+    }
+
+}
